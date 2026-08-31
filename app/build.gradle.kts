fdf2718c3ba47507feceb222d0fab4823fd3b3e7
Vstory <20795557+Vstory@users.noreply.github.com>
2026-08-31 06:17:35 +0000

    docs: 关于页补充基于LSPosed API 102改造说明 (v1.9(33))

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionName = "1.9"
-        versionCode = 32
+        versionCode = 33
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
